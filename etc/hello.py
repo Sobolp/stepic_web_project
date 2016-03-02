13aea487c7181c8f5667dbc87176c44f3ba06edd
Sobolp <pbolotov@gmail.com>
2016-03-02 23:52:04 +0200

    v1.03

--- etc/hello.py
+++ etc/hello.py
@@ -1,2 +1,11 @@
-bind="0.0.0.0:8080,127.0.0.1:8080"
-hello:app
+CONFIG = {
+    # 'mode': 'wsgi',
+    'working_dir': '/home/box',
+    # 'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080,127.0.0.1:8080',
+        '--workers=16',
+        '--timeout=60',
+        'app.hello',
+    ),
+}
