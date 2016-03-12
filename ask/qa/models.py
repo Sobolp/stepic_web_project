5765edfd5fde28558fdebc05ad19c75b31f77910
Sobolp <pbolotov@gmail.com>
2016-03-13 00:09:58 +0200

    v3.04

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-# Question - вопрос
-# title - заголовок вопроса
-# text - полный текст вопроса
-# added_at - дата добавления вопроса
-# rating - рейтинг вопроса (число)
-# author - автор вопроса
-# likes - список пользователей, поставивших "лайк"
-
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -17,14 +9,9 @@ class Question(models.Model):
     added_at = models.DateTimeField(blank=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name='+')
 
 
-# Answer - ответ
-# text - текст ответа
-# added_at - дата добавления ответа
-# question - вопрос, к которому относится ответ
-# author - автор ответа
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank=True)
