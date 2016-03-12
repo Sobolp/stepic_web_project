4476ee1711cb6eb25a467f2edd91453658821e66
Sobolp <pbolotov@gmail.com>
2016-03-12 22:47:24 +0200

    v3.01

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -55,13 +55,20 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'my.cnf',
+        },
     }
 }
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# Question - вопрос
+# title - заголовок вопроса
+# text - полный текст вопроса
+# added_at - дата добавления вопроса
+# rating - рейтинг вопроса (число)
+# author - автор вопроса
+# likes - список пользователей, поставивших "лайк"
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+
+# Answer - ответ
+# text - текст ответа
+# added_at - дата добавления ответа
+# question - вопрос, к которому относится ответ
+# author - автор ответа
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
